1257d40f39cdd569a08d24ccd050e2ef93ed788e
mincheolkang34 <mincheolkang34@gmail.com>
2025-11-12 10:29:11 +0900

    publish test 1

--- src/test/java/kr/co/bnkfirst/BnkFirstApplicationTests.java
+++ src/test/java/kr/co/bnkfirst/BnkFirstApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@MapperScan("kr.co.bnkfirst.mapper")
-@DataJpaTest
 class BnkFirstApplicationTests {
 
     @Test
