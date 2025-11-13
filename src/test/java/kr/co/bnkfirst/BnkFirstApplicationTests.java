578d946c0c4cb3653c73c45b2d710b0928c78f18
sy7521012 <sy7521012@naver.com>
2025-11-13 16:18:02 +0900

    test 파일 충돌 해결

--- src/test/java/kr/co/bnkfirst/BnkFirstApplicationTests.java
+++ src/test/java/kr/co/bnkfirst/BnkFirstApplicationTests.java
@@ -7,10 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
-<<<<<<< HEAD
 @MapperScan("kr.co.bnkfirst.mapper")
-=======
->>>>>>> dd4bf84ebca73d85f136d06577f6e424a9f42e97
 class BnkFirstApplicationTests {
     @Test
     void contextLoads() {
