7d4b0502871eda53b9e1fab34617763dddafb6bf
mincheolkang34 <mincheolkang34@gmail.com>
2025-11-25 16:18:46 +0900

    add test file

--- src/main/java/kr/co/bnkfirst/BnkFirstApplication.java
+++ src/main/java/kr/co/bnkfirst/BnkFirstApplication.java
@@ -25,6 +25,24 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 //                }
 //        )
 //})
+// DB증권 API 토큰 발행 이슈 해결될 때까지 프로젝트에서 제외
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(
+                type = FilterType.REGEX,
+                pattern = {
+                        "kr\\.co\\.bnkfirst\\.dbstock\\..*",
+                        "kr\\.co\\.bnkfirst\\.dbstockrank\\..*",
+//                        "kr\\.co\\.bnkfirst\\.config\\.WebSocketConfig"
+                }
+        ),
+        @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = {
+//                        kr.co.bnkfirst.controller.StockController.class,
+//                        kr.co.bnkfirst.controller.KiwoomTestController.class
+                }
+        )
+})
 @EnableScheduling
 public class BnkFirstApplication {
 
